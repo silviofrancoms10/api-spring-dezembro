3ce4b39feb496d4720876bf1ac28683469d61e38
silviofranco <silviofrancoms@gmail.com>
2024-12-23 14:04:39 -0400

    classe user resource findById

--- src/main/java/br/com/silviofrancoms/apispringdezembro/resources/UserResource.java
+++ src/main/java/br/com/silviofrancoms/apispringdezembro/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.silviofrancoms.apispringdezembro.resources;
 
 import br.com.silviofrancoms.apispringdezembro.domain.User;
+import br.com.silviofrancoms.apispringdezembro.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1 , "Silvio", "F2oOw@example.com", "1234"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
