025889e1c0c74023130a0231bb92aa2051f5ffcc
silviofranco <silviofrancoms@gmail.com>
2024-12-23 18:59:18 -0400

    tratamento de exception para objeto não encontrado

--- src/main/java/br/com/silviofrancoms/apispringdezembro/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/silviofrancoms/apispringdezembro/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.silviofrancoms.apispringdezembro.services.impl;
 import br.com.silviofrancoms.apispringdezembro.domain.User;
 import br.com.silviofrancoms.apispringdezembro.repositories.UserRepository;
 import br.com.silviofrancoms.apispringdezembro.services.UserService;
+import br.com.silviofrancoms.apispringdezembro.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
     }
 }
