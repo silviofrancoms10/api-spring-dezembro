3efa962071ae054304050f5d9c84954d4a1e1204
silviofranco <silviofrancoms@gmail.com>
2024-12-23 13:00:05 -0400

    iniciando projeto

---
+++ src/main/java/br/com/silviofrancoms/apispringdezembro/ApiSpringDezembroApplication.java
@@ -0,0 +1,13 @@
+package br.com.silviofrancoms.apispringdezembro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringDezembroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringDezembroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/silviofrancoms/apispringdezembro/ApiSpringDezembroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.silviofrancoms.apispringdezembro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringDezembroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
